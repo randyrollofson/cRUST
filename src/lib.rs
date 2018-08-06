9e14c9f70ccc5031e984e2cc97458d029faa4ba6
Randy Rollofson <randyrollofson@gmail.com>
2018-08-06 11:47:13 -0700

    wip; adding distortion and overdrive

--- src/lib.rs
+++ src/lib.rs
@@ -13,6 +13,8 @@ struct Crust {
     sample_rate: f64,
     volume: f32,
     wave_index: f32,
+    distortion: f32,
+    overdrive: f32,
 }
 
 impl Default for Crust {
@@ -23,6 +25,8 @@ impl Default for Crust {
             sample_rate: 48000.0,
             volume: 0.0,
             wave_index: 0.0,
+            distortion: 0.0,
+            overdrive: 0.0,
         }
     }
 }
@@ -37,9 +41,9 @@ fn sawtooth_wave(midi_note: u8, volume: f32, time: f64) -> f32 {
 
 fn square_wave(midi_note: u8, volume: f32, time: f64) -> f32 {
     if (time * midi_note_num_to_freq(midi_note) * 2.0 * PI).sin() as f32 >= 0.0 {
-        volume * 1.0
+        volume * 0.4 // not using 1.0 in order to balance with other waveforms
     } else {
-        volume * -1.0
+        volume * -0.4
     }
 }
 
@@ -49,6 +53,34 @@ fn triangle_wave(midi_note: u8, volume: f32, time: f64) -> f32 {
 
 fn midi_note_num_to_freq(midi_note_number: u8) -> f64 {
     ((midi_note_number as f64 - 69.0) / 12.0).exp2() * 440.0
+}
+
+
+// Distortion formula based on
+// https://ccrma.stanford.edu/~orchi/Documents/DAFx.pdf
+fn distortion(input: f32, distortion:f32) -> f32 {
+    let gain = 5.0;
+    let q = input / input.abs();
+    let y = q * (1.0 - (gain * (q * input)).exp());
+    let z = distortion * y + (1.0 - distortion) * input;
+    z
+}
+
+// Overdrive formula based on
+// https://ccrma.stanford.edu/~orchi/Documents/DAFx.pdf
+fn overdrive(input: f32) -> f32 {
+    let output: f32;
+    if input < 0.33 {
+        output = 2.0 * input;
+    } else if input >= 0.33 && input < 0.66 {
+        output = (3.0 - (2.0 - (3.0 * input)).exp2()) / 3.0;
+    } else if input >= 0.66 && input <= 1.0 {
+        output = 1.0;
+    } else {
+        output = input;
+    }
+
+    output
 }
 
 impl Crust {
@@ -78,7 +110,7 @@ impl Plugin for Crust {
             unique_id: 736251,
             inputs: 2,
             outputs: 2,
-            parameters: 2,
+            parameters: 4,
             category: Category::Synth,
             ..Default::default()
         }
@@ -88,6 +120,8 @@ impl Plugin for Crust {
         match index {
             0 => self.wave_index,
             1 => self.volume,
+            2 => self.distortion,
+            3 => self.overdrive,
             _ => 0.0,
         }
     }
@@ -97,6 +131,8 @@ impl Plugin for Crust {
         match index {
             0 => self.wave_index = val,
             1 => self.volume = val,
+            2 => self.distortion = val,
+            3 => self.overdrive = val,
             _ => (),
         }
     }
@@ -105,6 +141,8 @@ impl Plugin for Crust {
         match index {
             0 => "waveform".to_string(),
             1 => "volume".to_string(),
+            2 => "distortion".to_string(),
+            3 => "overdrive".to_string(),
             _ => "".to_string(),
         }
     }
@@ -113,6 +151,8 @@ impl Plugin for Crust {
         match index {
             0 => format!("{}", ((self.wave_index) * 4.0).round()),
             1 => format!("{}%", ((self.volume) * 100.0).round()),
+            2 => format!("{}", ((self.distortion) * 10.0).round()),
+            2 => format!("{}", ((self.overdrive) * 10.0).round()),
             _ => "".to_string(),
         }
     }
@@ -137,17 +177,25 @@ impl Plugin for Crust {
             for (_, output_sample) in input_buffer.iter().zip(output_buffer) {
 
                 if self.midi_note != 0 {
+                    let wave;
+                    //let dist;
                     if self.wave_index >= 0.0 && self.wave_index < 0.25 {
-                        *output_sample = sine_wave(self.midi_note, volume, time);
+                        wave = sine_wave(self.midi_note, volume, time);
                     } else if self.wave_index >= 0.25 && self.wave_index < 0.5 {
-                        *output_sample = sawtooth_wave(self.midi_note, volume, time);
+                        wave = sawtooth_wave(self.midi_note, volume, time);
                     } else if self.wave_index >= 0.5 && self.wave_index < 0.75 {
-                        *output_sample = square_wave(self.midi_note, volume, time);
+                        wave = square_wave(self.midi_note, volume, time);
                     } else if self.wave_index >= 0.75 && self.wave_index <= 1.0 {
-                         *output_sample = triangle_wave(self.midi_note, volume, time);
+                         wave = triangle_wave(self.midi_note, volume, time);
                     } else {
-                         *output_sample = 0.0;
+                         wave = 0.0;
                     }
+
+                    //*output_sample = distortion(wave, self.crust);
+                    //*output_sample = wave;
+
+                    *output_sample = overdrive(wave);
+
                     time += sample;
                 } else {
                     *output_sample = 0.0;
